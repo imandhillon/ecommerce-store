ddf3bb8a8da40ede8864cf2e0b148b9f974ce1e4
Iman <imansingh96@gmail.com>
2020-01-19 18:26:27 -0500

    First commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import 'bootstrap'
+import './assets/app.scss'
+
+import jQuery from 'jquery'
+window.$ = window.jQuery = jQuery
+
+import 'popper.js'
+
+Vue.component('Navbar', require('./components/Navbar.vue').default)
 
 Vue.config.productionTip = false
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
-  <div class="home">
+  <div class="home container">
+    <!-- <Navbar></Navbar>
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+    <button class="btn btn-primary">click</button> -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
+import Navbar from '@/components/Navbar.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    // HelloWorld,
+    Navbar
   }
 }
 </script>
